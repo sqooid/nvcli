8482cec512040fd7d2a7cc573040a587ec037ea2
Lucas Liu <sqooiddev@gmail.com>
2022-07-14 22:14:01 +1000

    added status error conversion

--- src/nvapi/display.rs
+++ src/nvapi/display.rs
@@ -3,14 +3,13 @@ use std::fmt::Display;
 use nvapi_sys_new::{
     make_nvapi_version, NvAPI_DISP_GetDisplayConfig, NvAPI_DISP_SetDisplayConfig,
     NvAPI_GPU_GetConnectedDisplayIds, NvPhysicalGpuHandle, _NvAPI_Status_NVAPI_OK,
-    NV_DISPLAYCONFIG_PATH_ADVANCED_TARGET_INFO,
-    NV_DISPLAYCONFIG_PATH_INFO, NV_DISPLAYCONFIG_PATH_TARGET_INFO_V2,
-    NV_DISPLAYCONFIG_SOURCE_MODE_INFO_V1, NV_GPU_DISPLAYIDS,
+    NV_DISPLAYCONFIG_PATH_ADVANCED_TARGET_INFO, NV_DISPLAYCONFIG_PATH_INFO,
+    NV_DISPLAYCONFIG_PATH_TARGET_INFO_V2, NV_DISPLAYCONFIG_SOURCE_MODE_INFO_V1, NV_GPU_DISPLAYIDS,
 };
 
 use crate::cli::error::Result;
 
-use super::scaling::Scaling;
+use super::{general::get_status_message, scaling::Scaling};
 
 pub unsafe fn get_display_ids(gpu_handle: NvPhysicalGpuHandle) -> Vec<NV_GPU_DISPLAYIDS> {
     let mut display_ids: Vec<NV_GPU_DISPLAYIDS>;
@@ -121,7 +120,10 @@ pub fn set_display_config(config: &mut Vec<NvDisplayConfigPathInfo>) -> Result<(
         result = NvAPI_DISP_SetDisplayConfig(config.len() as u32, path_info.as_mut_ptr(), 0);
     }
     if result != _NvAPI_Status_NVAPI_OK {
-        Err(format!("Failed to apply settings: error code {}", &result))
+        Err(format!(
+            "Failed to apply settings: {}",
+            get_status_message(&result)
+        ))
     } else {
         Ok(())
     }
--- src/nvapi/general.rs
+++ src/nvapi/general.rs
@@ -1,7 +1,21 @@
-use nvapi_sys_new::NvAPI_Initialize;
+use std::fmt::Display;
+
+use nvapi_sys_new::{NvAPI_GetErrorMessage, NvAPI_Initialize, _NvAPI_Status};
 
 pub fn initialize() {
     unsafe {
         NvAPI_Initialize();
+    }
+}
+
+pub fn get_status_message(status: &_NvAPI_Status) -> String {
+    let mut buffer: [i8; 64] = [0; 64];
+    let str_buffer;
+    unsafe {
+        NvAPI_GetErrorMessage(*status, buffer.as_mut_ptr());
+        str_buffer = std::mem::transmute::<[i8; 64], [u8; 64]>(buffer);
+        let mut message = std::str::from_utf8_unchecked(&str_buffer).to_owned();
+        message.truncate(message.find("\0").unwrap_or(64));
+        message
     }
 }
